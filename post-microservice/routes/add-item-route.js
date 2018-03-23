a956e1c2639aebb6a90adfa4529d08051b66498b
Anthony Chan <anthonychan0123@gmail.com>
2018-03-23 18:10:58 -0400

    Add JWT session verification

--- post-microservice/routes/add-item-route.js
+++ post-microservice/routes/add-item-route.js
@@ -1,6 +1,7 @@
 var express = require('express'),
  bodyParser = require('body-parser'),
  jwt = require('jsonwebtoken'),
+ fs = require('fs'),
  mongoose = require('mongoose'),
  mongoose_addItem = require('../mongoose/services/addItemService.js');
 
@@ -10,31 +11,33 @@ router.use(bodyParser.urlencoded({
     extended: false
 }));
 
-var secret;
+let configVars = fs.readFileSync('./config_vars.json');
+let obj = JSON.parse(configVars);
+var secret = obj.secret;
 var validContentTypes = [null, 'retweet','reply'];
 
 router.post('/additem', function(req, res){
 	id = mongoose.Types.ObjectId();
-	//var userJWT = jwt.verify(req.cookies.token, secret);
-	if (true){
-		if (req.body.content == null || !(new Set(validContentTypes).has(req.body.childType)))
-			res.send({status: "error", error: "unable to create user"});
-		else {
-			var item = mongoose_addItem.createItem({
-				//username: userJWT.username,
-				username: "test",
-				content: req.body.content,
-				childType: req.body.childType,
-				likes: 0,
-				retweeted: 0,
-				timestamp: Date.now(),
-				id: id
-			});
-			res.send({status: "OK", id: id});
+	var userJWT = jwt.verify(req.cookies.token, secret, function(err, decoded){
+		if (err){
+			res.send({status: "error", error: "invalid session"});
+		} else {
+			if (req.body.content == null || !(new Set(validContentTypes).has(req.body.childType)))
+				res.send({status: "error", error: "unable to create user"});
+			else {
+				var item = mongoose_addItem.createItem({
+					username: decoded.username,
+					content: req.body.content,
+					childType: req.body.childType,
+					likes: 0,
+					retweeted: 0,
+					timestamp: Date.now(),
+					id: id
+				});
+				res.send({status: "OK", id: id});
+			}
 		}
-	} else {
-		res.send({status: "error", error: "invalid session"});
-	}
+	})
 });
 
 module.exports = router;
