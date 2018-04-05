0e0745ecb39691981df5ff8befbd1c52c3e7d63c
Anthony Chan <anthonychan0123@gmail.com>
2018-04-04 23:58:48 -0400

    Add routes

--- follow-microservice/app.js
+++ follow-microservice/app.js
@@ -7,8 +7,12 @@ var app = express();
 app.set('port', process.env.PORT || 3002);
 app.use(cookieParser())
 app.set('configVars', configVars)
+
 // Routes
-app.use(require('./routes/add-item-route'));
+app.use(require('./routes/get-user-route'));
+app.use(require('./routes/get-followers-route'));
+app.use(require('./routes/get-following-route'));
+app.use(require('./routes/follow-route'));
 
 // Connect to MongoDB
 mongoose.connect('mongodb://'+ configVars.mongodb_host +':'+ configVars.mongodb_port+ '/' + configVars.mongodb_database);
